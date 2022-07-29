9280efc5d105dbeaddd341f8408274ab6680e301
Alexandre Junior <alexandrejunior408@gmail.com>
2022-07-28 22:59:35 -0300

    refatoração do index e separado por componentes

--- index.js
+++ index.js
@@ -3,31 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const User = require('./models/Person');
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post('/employee', async (req, res) => {
-  const { name, role, birthday_date, salary } = req.body;
-  const user = {
-    name,
-    role,
-    birthday_date,
-    salary,
-  };
+const userRoutes = require('./routes/userRoutes');
 
-  try {
-    await User.create(user);
-
-    res
-      .status(201)
-      .json({ message: 'Usuário inserido no sistema com sucesso!' });
-  } catch (error) {
-    res.status(500).json({ error: error });
-  }
-});
-app.get('/employee', async (req, res) => {});
+app.use('/employee', userRoutes);
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
